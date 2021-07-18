a7d5b244b43de483e3de60f6d738a1e6b4817934
jadessu <junioradessu@gmail.com>
2021-07-17 19:39:43 -0500

    create userview middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,10 +3,10 @@
 //     isLoggedIn,
 // }
 
-// function passUserToView (req, res, next){
-//     res.locals.user = req.user ? req.user : null
-//     next()
-// }
+function passUserToView (req, res, next){
+    res.locals.user = req.user ? req.user : null
+    next()
+}
 
 // function isLoggedIn(req, res, next){
 //     if (req.isAuthenticated()) return next()
