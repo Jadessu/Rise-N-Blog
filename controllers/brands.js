9d7b8c9581324f17f4e078ea9726c351e8552b28
jadessu <junioradessu@gmail.com>
2021-07-20 09:31:31 -0500

    recreate blog controller

--- controllers/brands.js
+++ controllers/brands.js
@@ -1,27 +1,16 @@
 import { Profile } from "../models/profile.js"
+import { User } from "../models/user.js"
 
 export{
     newBrand as new,
 }
 
+
+
 function newBrand(req, res){
-    // res.render("brands/new")
-    // console.log("this is a new brand statement")
-        console.log("this works")
-        Profile.findById(req.params.id)
-        .then(profile => {
-          Profile.findById(req.user.profile._id)
-          .then(self => {
-            const isSelf = self._id.equals(profile._id)
-            res.render("brands/new", {
-              profile,
-              self,
-              isSelf
-            })
-          })
-        })
-        .catch((err) => {
-          console.log(err)
-          res.redirect("/")
-        })
-    }
+  console.log("this better work, I am tired")
+  res.render("brands/new",{
+    Profile,
+    User
+  })
+}
