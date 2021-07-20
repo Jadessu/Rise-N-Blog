3d779e5fa0443d3a318aca43e7693740e550d306
jadessu <junioradessu@gmail.com>
2021-07-20 08:40:00 -0500

    change routes on server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as tipsRouter } from "./routes/tips.js"
 import { router as profilesRouter } from "./routes/profiles.js"
-import { router as brandsRouter } from "./routes/brands.js"
+import { router as brandsRouter} from "./routes/brands.js"
 
 
 // view engine setup
